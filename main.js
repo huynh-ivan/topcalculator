3da951b0f77d29bbb177d1dd97c9b9ee8a5be696
Ivan Huynh <32465307+huynh-ivan@users.noreply.github.com>
2023-11-27 14:03:28 -0500

    Wire up css + js docs and write the add function

---
+++ main.js
@@ -0,0 +1,22 @@
+// create the add function
+function add(operand1, operand2) {
+    return operand1 + operand2
+}
+
+// create the subtract function
+
+
+// create the multiply function
+
+
+// create the divide function
+
+
+// create an operate function that takes in two operands and an operator as parameters
+// it seems like there is conditional logic involved here
+// if the operator is `+`, do `add` function w/ the two arguments
+// if the operator is `-`, do `subtract` function w/ the two arguments
+// if the operator is `*`, do `multiply` function w/ the two arguments
+// if the operator is `/`, do `divide` function w/ the two arguments
+
+// make a button that calls the operate function when cliked or keydown'd
