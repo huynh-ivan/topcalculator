d5df85f58e8a5094c074d6ce8fc24a4a4d1c1989
Ivan Huynh <32465307+huynh-ivan@users.noreply.github.com>
2023-12-05 14:27:30 -0500

    separate error handling and clearing into their own functions

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 function add(firstNumber, secondNumber) {
-    displayOutput.textContent = firstNumber + secondNumber;
-    return firstNumber + secondNumber;
+    const sum = firstNumber + secondNumber;
+    displayOutput.textContent = sum;
+    return sum;
 }
 
 function subtract(firstNumber, secondNumber) {
-    displayOutput.textContent = firstNumber - secondNumber;
-    return firstNumber - secondNumber;
+    const remainder = firstNumber - secondNumber
+    displayOutput.textContent = remainder;
+    return remainder;
 }
 
 function multiply(firstNumber, secondNumber) {
-    displayOutput.textContent = firstNumber * secondNumber;
-    return firstNumber * secondNumber;
+    const product = firstNumber * secondNumber
+    displayOutput.textContent = product;
+    return product;
 }
 
 function divide(firstNumber, secondNumber) {
-    displayOutput.textContent = firstNumber / secondNumber;
-    return firstNumber / secondNumber;
+    const quotient = firstNumber / secondNumber
+    displayOutput.textContent = quotient;
+    return quotient;
 }
 
 // create an operate function that takes in two operands and an operator as parameters
 
-function operate(firstNumber, operator, secondNumber) {
+function operate(firstNumber = 0, operator, secondNumber = 0) {
+
     // Logic for which operation to perform with the parameters
     if (operator === `add`) {
         return result = add(firstNumber, secondNumber);
@@ -38,9 +43,30 @@ function operate(firstNumber, operator, secondNumber) {
         return result = divide(firstNumber, secondNumber);
     }
 
-    if (operator !== `add` && operator !== `subtract` && operator !== `multiply` && operator !== `divide` && operator !== `equals`) {
-        return `Invalid operator provided. +, - , * , / , = are valid operators`
+    if (operator !== `add` && operator !== `subtract` && operator !== `multiply` && operator !== `divide`) {
+        return handleInvalidOperator();
     }
+}
+
+function clearHistory() {
+    displayOutput.textContent = 0;
+    userInputValues = [];
+    currentOperand = 0;
+    storedOperand = 0;
+    operator = '';
+    resultHistory = [];
+    console.log('History was cleared')
+    console.log(resultHistory);
+}
+
+function handleError() {
+    console.log('Could not perform operation. Please try again')
+}
+
+function handleInvalidOperator() {
+    const errorMessage = `Invalid Operator. +, - , * , or / are valid operators`
+    console.log(errorMessage)
+    return errorMessage
 }
 
 function go() {
@@ -51,13 +77,11 @@ function go() {
     let operator = '';
     let resultHistory = [];
 
-
     //Get the number buttons
     const numberButtons = document.querySelectorAll('.numberButton');
     numberButtons.forEach(
         button => button.addEventListener('click', function (e) {
-            // if the resultsHistory is not empty
-            // if the results history is empty
+
             userInputValues.push(e.currentTarget.id);
             currentOperand = parseInt(userInputValues.join(''));
             displayOutput.textContent = currentOperand;
@@ -71,9 +95,8 @@ function go() {
         operatorButton.addEventListener('click', function () {
             // if the results history is not empty
             if (resultHistory.length > 0) {
-                operator = operatorButton.id;
-                console.log(operator);
                 storedOperand = resultHistory[resultHistory.length - 1];
+                operator = operatorButton.id;
                 return operator = operatorButton.id;
             } else {
                 // if the resultsHistory is empty, 
@@ -95,21 +118,12 @@ function go() {
         operator = '';
         //store the result in the resultHistory
         resultHistory.push(result);
-        console.log(result, resultHistory);
         return result;
     })
 
     //Get the clear button
     const clear = document.querySelector('#clear');
-    clear.addEventListener('click', function () {
-        console.clear();
-        displayOutput.textContent = 0;
-        userInputValues = [];
-        currentOperand = 0;
-        storedOperand = 0;
-        operator = '';
-        resultHistory = [];
-    })
+    clear.addEventListener('click', clearHistory);
 }
 
 //Get the displayOutput
